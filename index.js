b3282e48b3cef9f6b99f30d732e45563dd9759d2
Micah Evans <micah.s.evans@gmail.com>
2021-04-21 15:28:14 -0400

    Added a series of images and completed projectContainer3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    let projectContainers = ".projectContainer1, .projectContainer2";
+    let projectContainers = ".projectContainer1, .projectContainer2, .projectContainer3";
 
     $(projectContainers).hide();
 
@@ -20,6 +20,16 @@ $(document).ready(function () {
 
             $(".defaultContainer").hide("slow");
             $(".projectContainer2").show("slow");
+
+        });
+    });
+
+    $(function menuToggle3() {
+        $(".box3").click(function () {
+            event.preventDefault();
+
+            $(".defaultContainer").hide("slow");
+            $(".projectContainer3").show("slow");
 
         });
     });
